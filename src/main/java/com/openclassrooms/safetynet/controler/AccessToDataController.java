65e5fcf4edcd25ec9e461e416af238fb7f55a659
Silvio REA <silvio.rea07@gmail.com>
2022-01-17 15:50:49 +0100

    add AccessToDataController

---
+++ src/main/java/com/openclassrooms/safetynet/controler/AccessToDataController.java
@@ -0,0 +1,64 @@
+package com.openclassrooms.safetynet.controler;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.openclassrooms.safetynet.service.AccessToDataService;
+
+@RestController
+public class AccessToDataController
+{
+	@Autowired
+	AccessToDataService accessToDataService;
+	
+	@GetMapping("/firestation")
+	public List<String> getFirestationsFromPersons(@RequestParam String stationNumber)
+	{
+		return accessToDataService.getFirestationsFromPersons(stationNumber);
+	}
+	
+	
+	@GetMapping("/childAlert")
+	public List<String> getChildren(@RequestParam String address)
+	{
+		return accessToDataService.getChildren(address);
+
+	}
+	
+	@GetMapping("/phoneAlert")
+	public List<String> getPhoneNumbers(@RequestParam String firestation)
+	{
+		return accessToDataService.getPhoneNumbers(firestation);
+
+	}
+	
+	@GetMapping("/fire")
+	public List<String> getPersonsFromAdress(@RequestParam String address)
+	{
+		return accessToDataService.getPersonsFromAdress(address);
+	}
+	
+	@GetMapping("/flood")
+	public List<String> getPersonsFromStation(@RequestParam String station)
+	{
+		return accessToDataService.getPersonsFromStation(station);
+	}
+
+	@GetMapping("personInfo")
+	public List<String> getPersons()
+	{
+		return accessToDataService.getPersons();
+	}
+
+	@GetMapping("/communityEmail")
+	public List<String> getEmail()
+	{
+		return accessToDataService.getEmail();
+		
+	}
+
+}
